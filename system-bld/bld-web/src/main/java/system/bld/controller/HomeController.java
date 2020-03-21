93e5d1f43972fa6f88c1af228e8b7e95430043c2
litao <litao@17of,.com>
2020-03-21 18:00:25 +0800

    页面模板

--- system-bld/bld-web/src/main/java/system/bld/controller/HomeController.java
+++ system-bld/bld-web/src/main/java/system/bld/controller/HomeController.java
@@ -1,13 +1,18 @@
 package system.bld.controller;
 
 import org.apache.ibatis.annotations.Param;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import system.bld.entiry.UserEntity;
 import system.bld.service.UserService;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
 
 /**
  * Created with IntelliJ IDEA.
@@ -18,17 +23,23 @@ import javax.servlet.http.HttpServletRequest;
  * To change this template use File | Settings | File Templates.
  * Description:
  */
-@RestController
+@Controller
 @RequestMapping("/home")
 public class HomeController {
+	private static final Logger log= LoggerFactory.getLogger(HomeController.class);
 
 	@Autowired
 	UserService userService;
 
 	@RequestMapping("/index")
-	public String home(HttpServletRequest request, String string){
+	public String home(HttpServletRequest request, String string, Model model){
 
 		UserEntity result= userService.getUserById(1);
-		return "hello user info : ".concat(result.toString());
+		//return "hello user info : ".concat(result.toString());
+		log.info("------------------");
+		model.addAttribute("id","123");
+		model.addAttribute("name","litao");
+		model.addAttribute("desc","test");
+		return "index";
 	}
 }
